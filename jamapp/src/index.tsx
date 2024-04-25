a2b8452250c0f1131be1ceba40556b82f1f8679c
JamileDelmondes <159553162+JamileDelmondes@users.noreply.github.com>
2024-04-24 22:25:06 -0300

    att jamapp
    24/04/2024

---
+++ jamapp/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export default function SingnIn(){
+  return(
+    <View>
+      <Text>Hello World, ta na hora de codar</Text>
+    </View>
+  )
+}
