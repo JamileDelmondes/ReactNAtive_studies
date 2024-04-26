148f0c2e10cf7af2f176f3b030ed80814ca007c6
Diego-Costa-Nascimento <159553162+JamileDelmondes@users.noreply.github.com>
2024-04-26 11:58:50 -0300

    att jamapp
    26/04/2024

--- jamapp/src/screens/index.tsx
+++ jamapp/src/screens/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 
-export default function SingnIn(){
+export function SignIn(){
   return(
     <View>
       <Text>Hello World, ta na hora de codar</Text>
     </View>
-  )
+  );
 }
