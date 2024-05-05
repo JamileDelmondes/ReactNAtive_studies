85b16a4d5092926e80debd12f8fc4ac20c11d7f6
JamileDelmondes <159553162+JamileDelmondes@users.noreply.github.com>
2024-05-04 22:33:31 -0300

    att
    04/05/2024

--- jamapp/App.tsx
+++ jamapp/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { SingnIn } from './src/screens/SingnIn';
-
+import { SignIn } from './src/screens/index';
 
 export default function App(){
   return(
-    <SingnIn></SingnIn>
+    <SignIn></SignIn>
   )
 }
---
+++ jamapp/src/screens/index.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
+import { styles } from './styles';
+
+
+export function SignIn(){
+  const [text, setText] = useState ("Você não digitou nada ainda");
+
+  return(
+    <View style={styles.container}>
+      <Text>
+        Hello World, ta na hora de codar
+      </Text>
+      <TextInput style={styles.input}
+      onChangeText={setText}
+      />
+
+        <Text>
+          Você digitou: {text}
+        </Text>
+    </View>
+  )
+}
