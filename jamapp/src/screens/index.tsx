e45a8dd9e69dcf98a7092f25377a1814efdcf183
luaninhadev <159553162+JamileDelmondes@users.noreply.github.com>
2024-05-15 11:59:29 -0300

    att
    15/05/2024

--- jamapp/src/screens/index.tsx
+++ jamapp/src/screens/index.tsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Image } from 'react-native';
+import { View, Text, TextInput, Image,StatusBar } from 'react-native';
 import { styles } from './styles';
+
+
 import { IllustrationImg } from '../assets/illustration.png';
-import { StatusBar } from 'expo-status-bar';
+import { ButtonIcon } from '../components/ButtonIcon';
 
 
 export function SignIn(){
-  const [text, setText] = useState ("Você não digitou nada ainda");
+  const [text,setText] = useState ("Você não digitou nada ainda");
 
   return(
     <View style={styles.container}>
       <StatusBar barStyle='light-content' backgroundColor={"transparent"} translucent />
-        <Image source={IllustrationImg} style={styles.image} resizeMode='stretch'/>
+      <Image source={IllustrationImg} style={styles.image} resizeMode='stretch'/>
     <View style= {styles.content}>
       <Text style={styles.title}>
         Organize Suas {`\n`}
@@ -22,6 +24,7 @@ export function SignIn(){
         Crie grupos para jogar seus games{`\n`}
         favoritos com seus amigos
       </Text>
+      <ButtonIcon/>
 
 
     </View>
--- jamapp/src/screens/styles.ts
+++ jamapp/src/screens/styles.ts
@@ -4,14 +4,25 @@ import { theme } from '../global/styles/theme';
 export const styles = StyleSheet.create({
     container:{
     flex:1,
-    backgroundColor: 'red',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundcolor: theme.colors.cordefundo   
+    backgroundColor: theme.colors.cordefundo   
     },
     image: {
         width:'100%',
         height:360
+    },
+    content:{
+        marginTop: -40
+    },
+    title:{
+        color:theme.colors.headin
+    },
+    subtitle:{
+        color:theme.colors.headin,
+        fontSize:15,
+        textAlign:'center',
+        marginBottom:64
     }
 })
 
