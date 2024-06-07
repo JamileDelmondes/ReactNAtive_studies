bde925d690fcfdefe54625b4d13b878134a54972
darkznn10 <159553162+JamileDelmondes@users.noreply.github.com>
2024-06-07 11:56:49 -0300

    att
    07/06/2024

--- jamapp/src/global/styles/theme.ts
+++ jamapp/src/global/styles/theme.ts
@@ -2,5 +2,14 @@ export const theme = {
     colors:{
         cordefundo: '#0D133D',
         headin: '#DDE3F0',
-    }
+        primary: '#E51C44',
+        line: '#3991F36'
+    },
+
+fonts:{
+    title700: 'radjhani_700Bold',
+    title500: 'radjhani_500Medium',
+    text400: 'Inter_400Regular',
+    text500: 'Inter_500Medium',
+}
 }
